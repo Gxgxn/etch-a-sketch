ab44a9244ca270376280479b95fe360b386aac60
Gxgxn <gdsinghb99@gmail.com>
2022-10-09 18:03:49 -0300

    added controls and the grid fully working

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-let gridContainer = document.getElementById("container");
+let gridContainer = document.getElementById("container-cell");
 let colValue = 16;
-
+let gridRange = document.getElementById("size");
+let createGridBtn = document.querySelector("#create-grid");
+let gridActive = false;
 //creating grid
 function createGrid(colValue) {
   for (i = 1; i < colValue * colValue; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
+    gridContainer.style.gridTemplateColumns = `repeat(${colValue}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${colValue}, 1fr)`;
     gridContainer.appendChild(cell);
   }
 }
-createGrid(16);
+//default population
+createGrid(15);
 
-//adding eventlistener to each element
-let cells = document.querySelectorAll(".cell"); //returns an array
+//On click listner to start/stop process etching
 
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", (e) => {
-    e.target.style.backgroundColor = "#e74c3c";
-    console.log("f");
-  });
+gridContainer.addEventListener("click", () => {
+  captureCells();
+  gridActive = !gridActive;
 });
+
+//event for value range
+gridRange.addEventListener("change", () => {
+  let label = document.getElementById("size-display");
+  label.innerText = `Grid-Size: ${gridRange.value}`;
+});
+//grid change eventLisnter
+createGridBtn.addEventListener("click", () => {
+  removeGrid();
+  createGrid(gridRange.value);
+});
+
+//clears populated grid
+function removeGrid() {
+  document.querySelectorAll(".cell").forEach((cell) => cell.remove());
+}
+
+//add event listener to created elements
+function captureCells() {
+  let cells = document.querySelectorAll(".cell"); //returns an array
+
+  cells.forEach((cell) => {
+    if (!gridActive) {
+      cell.addEventListener("mouseenter", colorChange);
+    } else {
+      cell.removeEventListener("mouseenter", colorChange);
+    }
+  });
+}
+
+function colorChange(e) {
+  e.target.style.backgroundColor = "#e74c3c";
+}
